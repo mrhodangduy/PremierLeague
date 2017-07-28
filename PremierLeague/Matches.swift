20c9968345d16f6b65d9df3473f1e35b96aa7d81
QTS Coder <tester.qts@gmail.com>
2017-07-28 16:30:20 +0700

    first commit

---
+++ PremierLeague/Matches.swift
@@ -0,0 +1,106 @@
+//
+//  Matches.swift
+//  PremierLeague
+//
+//  Created by QTS Coder on 7/28/17.
+//  Copyright © 2017 David. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Match {
+    let date:String?
+    let status:String?
+    let matchday:Int?
+    let homeTeamName:String?
+    let awayTeamName:String?
+    let result:[String:AnyObject]
+    
+    enum sync:Error {
+        case missing(String)
+        case invalid(String)
+    }
+    
+    init (json: [String:AnyObject]) throws
+    {
+        guard let date = json["date"] as? String else { throw sync.missing("date is missing")}
+        guard let status = json["status"] as? String else { throw sync.missing("status is missing")}
+        guard let matchday = json["matchday"] as? Int else { throw sync.missing("matchday is missing")}
+        guard let homeTeamName = json["homeTeamName"] as? String else { throw sync.missing("homeTeamName is missing")}
+        guard let awayTeamName = json["awayTeamName"] as? String else { throw sync.missing("awayTeamName is missing")}
+        guard let result = json["result"] as? [String:AnyObject] else { throw sync.missing("result is missing")}
+        
+        self.date = date
+        self.status = status
+        self.matchday = matchday
+        self.homeTeamName = homeTeamName
+        self.awayTeamName = awayTeamName
+        self.result = result
+    }
+    
+    static let basePath:String = "http://api.football-data.org/v1/competitions/445/fixtures"
+    
+    static func matchList (completion: @escaping ([Match]?) -> ())
+    {
+        let url = URL(string: basePath)
+        let task = URLSession.shared.dataTask(with: url!) { (data, respone, error) in
+            
+            var arrayMatchList = [Match]()
+            
+            if error != nil
+            {
+                print(error!)
+            } else
+            {
+                if let content = data
+                {
+                    do
+                    {
+                        let myjson = try JSONSerialization.jsonObject(with: content, options: .mutableContainers) as? [String: AnyObject]
+                        print(myjson?["date"]!)
+                        
+//                        let listMatches = myjson as? Array<[String:AnyObject]>
+//                        
+//                        for matches in myjson!
+//                        {
+//                            if let matchObject = try? Match(json: matches)
+//                            {
+//                                arrayMatchList.append(matchObject)
+//                                print("\(arrayMatchList)\n\n")
+//                            }
+//                        }
+//                        
+                    } catch
+                    {
+                        print("loi")
+                    }
+                    
+                    completion(arrayMatchList)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PremierLeague/MatchesTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MatchesTableViewCell.swift
+//  PremierLeague
+//
+//  Created by QTS Coder on 7/28/17.
+//  Copyright © 2017 David. All rights reserved.
+//
+
+import UIKit
+
+class MatchesTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var lblHometeam: UILabel!
+    @IBOutlet weak var lblAwayteam: UILabel!
+    @IBOutlet weak var lblHomeScore: UILabel!
+    @IBOutlet weak var lblAwayScore: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
